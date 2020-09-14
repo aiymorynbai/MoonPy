81ea0e0a26cb590c57cfb844628fbf33c85525bf
aiym <aiym.orynbai@gmail.com>
2020-09-14 21:34:23 +0200

    structuring the project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='moonpy',
+    packages=find_packages(),
+    version='0.1.0',
+    description='random collection of useful code',
+    author='Aiym Orynbai',
+    license='?')
